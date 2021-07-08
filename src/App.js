927cdb6271a73c9e8120ce2e108006843536247d
jeo19 <terrakkk@outlook.com>
2021-07-08 23:16:05 +0800

    feat:Add the css module(install the react-icons module) step02

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import { useState } from "react";
-import CheckBox from "./components/CheckBox"
+import React, { useState } from 'react';
+import CheckBox from './components/CheckBox';
 
 function App() {
-  const [check,setCheck]=useState(false)
-  const onChange=e=>{
-    setCheck(e.target.checked)
-  }
-  return <div className="App">
-    <CheckBox onChange={onChange} checked={check}>Agreement</CheckBox>
-    <p>
-      <b>{check?"true":"false"}</b>
-    </p>
-  </div>;
+  const [check, setCheck] = useState(false);
+  const onChange = (e) => {
+    setCheck(e.target.checked);
+  };
+  return (
+    <div className="App">
+      <CheckBox onChange={onChange} checked={check}>
+        Agreement
+      </CheckBox>
+      <p>
+        <b>{check ? 'true' : 'false'}</b>
+      </p>
+    </div>
+  );
 }
 
 export default App;
--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { MdCheckBox, MdCheckBoxOutlineBlank } from 'react-icons/md';
 
 function CheckBox({ children, checked, ...rest }) {
   return (
     <div>
       <label>
         <input type="checkbox" checked={checked} {...rest} />
-        <div>{checked ? 'checked' : 'unchecked'}</div>
+        <div>{checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}</div>
       </label>
       <span>{children}</span>
     </div>
