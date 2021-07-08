65faf5c3cdd9f6e801d61ef9ee3607c13cfa54c0
jeo19 <terrakkk@outlook.com>
2021-07-09 01:00:22 +0800

    feat:Add the css module(apply the css module technique) step03

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { MdCheckBox, MdCheckBoxOutlineBlank } from 'react-icons/md';
+import styles from './CheckBox.module.css';
 
 function CheckBox({ children, checked, ...rest }) {
   return (
-    <div>
+    <div className={styles.checkbox}>
       <label>
         <input type="checkbox" checked={checked} {...rest} />
-        <div>{checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}</div>
+        <div className={styles.icon}>
+          {checked ? <MdCheckBox className={styles.checked} /> : <MdCheckBoxOutlineBlank />}
+        </div>
       </label>
       <span>{children}</span>
     </div>
