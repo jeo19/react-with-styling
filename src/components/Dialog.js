c46f37bfdc5eb7abc767d6c6f53924a5746e919e
jeo19 <terrakkk@outlook.com>
2021-08-02 23:41:20 +0800

    feat:Implement the dialog box disappear step06

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled, { keyframes } from 'styled-components';
 import Button from './Button';
 
@@ -59,7 +59,16 @@ const ShortMarginButton = styled(Button)`
 `;
 
 function Dialog({ title, children, confirmText, cancelText, onConfirm, onCancel, visible }) {
-  if (!visible) return null;
+  const [animate, setAnimate] = useState(false);
+  const [localVisible, setLocalVisible] = useState(visible);
+  useEffect(() => {
+    if (localVisible && !visible) {
+      setAnimate(true);
+      setTimeout(() => setAnimate(false), 250);
+    }
+    setLocalVisible(visible);
+  }, [localVisible, visible]);
+  if (!animate && !localVisible) return null;
   return (
     <DarkBackground>
       <DialogBlock>
